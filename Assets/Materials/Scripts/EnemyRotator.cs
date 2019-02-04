504e266c9b1a7a26ffef22c90c0d1ed121d14af8
rhiannadoesart <46513874+rhiannadoesart@users.noreply.github.com>
2019-02-03 23:49:05 -0500

    challenge asset folder

---
+++ Assets/Materials/Scripts/EnemyRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotator : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(-45, 30, -30) * Time.deltaTime);
+    }
+}
--- Assets/Materials/Scripts/PlayerController.cs
+++ Assets/Materials/Scripts/PlayerController.cs
@@ -8,15 +8,18 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text scoreText;//changed
 
     private Rigidbody rb;
     private int count;
+    private int score;//changed
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText ();
+        score = 0;//changed
+        SetAllText ();
         winText.text = "";
     }
     void FixedUpdate ()
@@ -38,15 +41,42 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText();
+            score = score + 1;
+            SetAllText();//changed
+        }
+        else if (other.gameObject.CompareTag("Enemy"))//changed and added
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score - 1;
+            SetAllText();//changed           
         }
     }
-    void SetCountText ()
+    
+    void SetAllText ()//changed
     {
-        countText.text = "Count:" + count.ToString();
+       // scoreText.text = "Winnings: " + score.ToString();
+        countText.text = "Consumption Score:" + count.ToString();
         if (count >=12)
         {
-            winText.text = "You Win";
-        }
+            winText.text = "The Game Has Been Won";
+        }        
     }
 }
+/*rivate void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.CompareTag("Pick Up"))
+    {
+        other.gameObject.SetActive(false);
+        count = count + 1;
+        score = score + 1; // I added this code to track the score and count separately.
+        SetAllText();
+    }
+    else if (other.gameObject.CompareTag("Enemy"))
+    {
+        other.gameObject.SetActive(false);
+        count = count + 1;
+        score = score - 1; // this removes 1 from the score
+        SetAllText();
+    }
+}*/
